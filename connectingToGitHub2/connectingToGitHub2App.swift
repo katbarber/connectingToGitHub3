a417fb79f93f367d504d25194878828abc229c54
Kat <kbarber876@gmail.com>
2024-06-26 09:53:13 -0400

    Initial Commit

---
+++ connectingToGitHub2/connectingToGitHub2App.swift
@@ -0,0 +1,17 @@
+//
+//  connectingToGitHub2App.swift
+//  connectingToGitHub2
+//
+//  Created by Kat Barber on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct connectingToGitHub2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
